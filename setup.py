eaa150e09b00702c90c3755356ab9e8e08335488
Chih-Pin Tan <tanchihpin0517@gmail.com>
2024-06-13 06:54:42 +0000

    fix import error of librosa

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['matplotlib', 'mido', 'librosa', 'torchlibrosa'],
-    python_requires='>=3.6',
+    install_requires=['matplotlib', 'mido', 'librosa>=0.9.0', 'torchlibrosa', 'resampy'],
+    python_requires='>=3.8',
 )
